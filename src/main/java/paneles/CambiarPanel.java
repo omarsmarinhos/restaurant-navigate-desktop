3b1247e0f8abb5492cdc77d619ed88cf83e9830f
Luis Bautista Machuca <luiscarlosbautistamachuca@gmail.com>
2023-01-24 22:11:53 -0500

    Diseño UI aun falta la mitad

---
+++ src/main/java/paneles/CambiarPanel.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paneles;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author MenDark
+ */
+public class CambiarPanel {
+    private JPanel container;
+    private JPanel content;
+    /**
+     * Constructor de clase
+     */
+    public CambiarPanel(JPanel container, JPanel content) {
+        this.container = container;
+        this.content = content;
+        this.container.removeAll();
+        this.container.revalidate();
+        this.container.repaint();
+        
+        this.container.add(this.content);
+        this.container.revalidate();
+        this.container.repaint();
+    }
+    
+}
